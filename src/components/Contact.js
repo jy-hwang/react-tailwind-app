7e4a993f0a2df4dd6e6655b1d5ce67e2701a028e
jy-hwang <stratojy@gmail.com>
2024-05-28 19:49:49 +0900

    Contact Component 생성
    1. Contact Component 생성

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,49 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <section name='contact' className='w-full h-screen p-4 bg-gradient-to-b'>
+
+            <div className='flex flex-col justify-center h-full max-w-screen-lg p-4 mx-auto'>
+
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold'>Contact</h2>
+                    <p className='py-6 text-3xl text-gray-500'>The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                </div>
+
+                <div className='flex items-center justify-center mt-10'>
+
+                    <form className='flex flex-col w-full md:w-1/2'>
+
+                        <input
+                            type='text'
+                            name='name'
+                            placeholder='Enter your name'
+                            className='p-2 border-2 rounded-md focus:outline-none'
+                        />
+
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder='Enter your email'
+                            className='p-2 border-2 mt-4 rounded-md focus:outline-none'
+                        />
+
+                        <textarea
+                            name='message'
+                            placeholder='Enter your message'
+                            rows='10'
+                            className='p-2 border-2 mt-4 rounded-md focus:outline-none'
+                        ></textarea>
+
+                        <button className='duration-300 bg-gradient-to-l from-cyan-300 to-blue-500 hover:scale-110 my-8 mx-auto px-6 py-3'>Let's Talk</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </section>
     )
 }
 
